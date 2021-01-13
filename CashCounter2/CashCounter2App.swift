15ca6a76f31a530afb85064634bfdab7db316e36
Will Prescott <will@theprescotts.com>
2021-01-13 13:27:02 -0600

    Initial Commit

---
+++ CashCounter2/CashCounter2App.swift
@@ -0,0 +1,17 @@
+//
+//  CashCounter2App.swift
+//  CashCounter2
+//
+//  Created by William Prescott on 2021-01-13.
+//
+
+import SwiftUI
+
+@main
+struct CashCounter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
